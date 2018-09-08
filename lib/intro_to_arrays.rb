8480c4125bf904df2edfe281dcc79a7e1ba8e523
Rajvir Rai <rsr1992@gmail.com>
2018-09-08 01:01:44 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,14 @@
+def instantiate_new_array
+  Array.new
+end
+
+
+def array_with_two_elements(array)
+  array = Array.new(2)
+end
+
+
+def first_element
+  first_element = ["Welcome to New York", "Hello", "Yo"]
+  return first_element.first
+end
